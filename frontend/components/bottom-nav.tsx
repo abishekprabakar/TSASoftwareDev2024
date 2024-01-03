b6901bf2d26c188226e0120db76f5ec8e989058d
Yubo Cao <cao2006721@gmail.com>
2024-01-02 21:50:34 -0500

    fix(frontend): :bug: Ensure all the usage of zinc and indigo are aligned with updated color style

--- frontend/components/bottom-nav.tsx
+++ frontend/components/bottom-nav.tsx
@@ -6,7 +6,7 @@ const BottomNav = () => {
 
   return (
     <div className="sm:hidden">
-      <nav className="fixed bottom-0 w-full border-t bg-zinc-100 pb-safe dark:border-zinc-800 dark:bg-zinc-900">
+      <nav className="fixed bottom-0 w-full border-t bg-dar-100 pb-safe dark:border-dar-800 dark:bg-dar-900">
         <div className="mx-auto flex h-16 max-w-md items-center justify-around px-6">
           {links.map(({ href, label, icon }) => (
             <Link
@@ -14,12 +14,12 @@ const BottomNav = () => {
               href={href}
               className={`flex h-full w-full flex-col items-center justify-center space-y-1 ${
                 router.pathname === href
-                  ? "text-indigo-500 dark:text-indigo-400"
-                  : "text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50"
+                  ? "text-pri-500 dark:text-pri-400"
+                  : "text-dar-600 hover:text-dar-900 dark:text-dar-400 dark:hover:text-dar-50"
               }`}
             >
               {icon}
-              <span className="text-xs text-zinc-600 dark:text-zinc-400">
+              <span className="text-xs text-dar-600 dark:text-dar-400">
                 {label}
               </span>
             </Link>
